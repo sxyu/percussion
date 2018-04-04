3094838fa374de807ddbfd980943b94e78053d34
Khang <knguyen0811@berkeley.edu>
2018-04-03 18:56:16 -0700

    added functionality for grabbing drumstick

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+	public OVRInput.Controller controller;
+
+	private float indexTriggerState = 0;
+	private float handTriggerState = 0;
+	private float oldIndexTriggerState = 0;
+
+	private bool holdingStick = false;
+	private GameObject stick = null;
+
+	public Vector3 holdPosition = new Vector3(0, -0.025f, 0.03f);
+	public Vector3 holdRotation = new Vector3(0, 180, 0);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		oldIndexTriggerState = indexTriggerState;
+		indexTriggerState = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller);
+		handTriggerState = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller);
+		if (holdingStick) {
+			if (handTriggerState < 0.6f) {
+				Release();
+			}
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (other.CompareTag("DrumStick")) {
+			if (handTriggerState > 0.9f && !holdingStick) {
+				Grab(other.gameObject);
+			}
+		}
+	}
+
+	void Grab(GameObject obj) {
+		holdingStick = true;
+		stick = obj;
+
+		stick.transform.parent = transform;
+		stick.transform.localPosition = holdPosition;
+		stick.transform.localEulerAngles = holdRotation;
+		stick.GetComponent<Rigidbody>().useGravity = false;
+		stick.GetComponent<Rigidbody>().isKinematic = true;
+	}
+
+	void Release() {
+		stick.transform.parent = null;
+
+		Rigidbody rigidbody = stick.GetComponent<Rigidbody>();
+
+		rigidbody.useGravity = true;
+		rigidbody.isKinematic = false;
+		rigidbody.velocity = OVRInput.GetLocalControllerVelocity(controller);
+
+		holdingStick = false;
+		stick = null;
+	}
+}
