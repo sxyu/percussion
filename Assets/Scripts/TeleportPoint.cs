616305b44329a00f2474b327ca066aed13bd48a3
Alex Yu <alexyu99126@gmail.com>
2018-04-24 17:25:03 -0700

    attempted to add teleportation system

--- Assets/Scripts/TeleportPoint.cs
+++ Assets/Scripts/TeleportPoint.cs
@@ -4,13 +4,73 @@ using UnityEngine;
 
 public class TeleportPoint : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // the position of the teleport point (where to teleport avatar to)
+    public Vector3 position;
+
+    // true if can teleport to this point
+    public bool canTeleport
+    {
+        get
+        {
+            return _canTeleport;
+        }
+        set
+        {
+            if (_canTeleport == value) return;
+            _canTeleport = value;
+            if (_canTeleport)
+            {
+                positions.Add(id, position);
+                beam.Play();
+            }
+            else
+            {
+                positions.Remove(id);
+                beam.Clear();
+                beam.Stop();
+            }
+        }
+    }
+    private bool _canTeleport;
+
+    // the particle 'beam' marking the teleport point
+    private ParticleSystem beam;
+
+    // records last used ID
+    private static int lastID = 0;
+
+    // ID of current TeleportPoint
+    private int id;
+
+    // stores all locations of teleport points
+    private static Dictionary<int, Vector3> positions = new Dictionary<int, Vector3>();
+
+    // Teleport 'avatar' to the nearest enabled TeleportPoint to 'point'
+    // if the distance between the two is below the given threshold
+    public static bool TeleportTo(Transform avatar, Vector3 point, float thresh = 0.2f)
+    {
+        float threshL2sqr = thresh * thresh;
+        float bestL2sqr = float.MaxValue;
+        int bestID = -1;
+
+        foreach (KeyValuePair<int, Vector3> entry in positions)
+        {
+            float l2sqr = (point - entry.Value).sqrMagnitude;
+            if (l2sqr <= threshL2sqr && l2sqr < bestL2sqr)
+            {
+                bestL2sqr = l2sqr;
+                bestID = entry.Key;
+            }
+        }
+
+        if (bestID == -1) return false;
+        avatar.position = point;
+        return true;
+    }
+
+    void Start () {
+        id = lastID++;
+        beam = GetComponent<ParticleSystem>();
+        positions.Add(id, position);
+    }
 }
---
+++ Assets/Scripts/TeleportRay.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportRay : MonoBehaviour {
+
+    public int numPoints = 50;
+    public float resolution = 0.1f;
+    public float gravity = 0.05f;
+    public Vector3 offset = new Vector3(0, 0.1f, 0);
+    private LineRenderer lineRenderer;
+
+    // Use this for initialization
+    void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 a = Camera.main.transform.position + offset;
+            Vector3 b = ray.GetPoint(resolution) + offset;
+            Vector3 delta = b - a;
+
+            Vector3[] pos = new Vector3[numPoints];
+            pos[0] = a; pos[1] = b;
+
+            float ressqr = delta.x * delta.x + delta.z * delta.z;
+
+            AnimationCurve curve = new AnimationCurve();
+
+
+            curve.AddKey(0.0f, 1.0f);
+            curve.AddKey(1.0f, 1.0f);
+
+            for (int i = 2; i < numPoints; ++i)
+            {
+                Vector3 v;
+                v.x = a.x + i * delta.x;
+                v.z = a.z + i * delta.z;
+                v.y = a.y + i * delta.y - i * i * ressqr * gravity;
+                pos[i] = v;
+                curve.AddKey(i, Vector3.Distance(v, a) / 1000.0f);
+            }
+            lineRenderer.positionCount = numPoints;
+            lineRenderer.widthCurve = curve;
+            lineRenderer.SetPositions(pos);
+        }
+        else
+        {
+            //lineRenderer.positionCount = 0;
+            //lineRenderer.SetPositions(new Vector3[]{ });
+        }
+    }
+}
